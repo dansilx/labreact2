7bbd35777777f608b729f1907bc8db5a81e9d2a8
dansilx <danilosilveira@msn.com>
2024-11-26 01:31:25 -0300

    enviando ex005

--- src/Gallery2.js
+++ src/Gallery2.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const images = [
-  { src: 'https://www.fullstackpython.com/img/logos/react.png', width: '50px', title: 'Image 1' },
+  { src: 'https://www.fullstackpython.com/img/logos/react.png', title: 'Image 1' },
   { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_rlMI9mJOXPOZeIt3SKr5YopXEnwPY_55sg&s', title: 'Image 2' },
   { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWbBKB9PQ37MtbmGDCA1YYWPz8X6zJkUcFkg&s', title: 'Image 3' },
 ];
@@ -19,7 +19,7 @@ function Gallery() {
       <img
         src={images[currentIndex].src}
         alt={images[currentIndex].title}
-        className="max-w-full max-h-52 object-contain mb-3"
+        className="w-40 h-40 object-cover mb-3 rounded-lg"
       />
       <button
         className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
